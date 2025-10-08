13097f60c10308a066e1a66f434685fc6d877ab3
manojLovesCoding <mkum9417@gmail.com>
2025-10-08 16:04:58 +0530

    fix(model): correct 'type' field in User schema to resolve Mongoose configuration error

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
-    _id: { types: String, required: true },
+    _id: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true },
     image: { type: String, required: true },
