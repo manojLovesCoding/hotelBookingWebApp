8c9ce2b77a16694240583c3006e1bed235339f27
manojLovesCoding <mkum9417@gmail.com>
2025-10-13 16:51:54 +0530

    project completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import AddRoom from "./pages/HotelOwner/AddRoom";
 import ListRoom from "./pages/HotelOwner/ListRoom";
 import { Toaster } from "react-hot-toast";
 import { useAppContext } from "./context/AppContext";
+import Loader from "./components/Loader";
 
 const App = () => {
   const isOwnerAuth = useLocation().pathname.includes("owner");
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/loader/:nextUrl" element={<Loader />} />
           <Route path="/owner" element={<Layout />}>
             <Route index element={<Dashboard />} />
             <Route path="add-room" element={<AddRoom />} />
